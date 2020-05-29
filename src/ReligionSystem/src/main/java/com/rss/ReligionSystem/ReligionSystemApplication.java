332e0f1f20416a5d2e862115d7433a340a6551b4
JeongJunKwon <37372602+JeongJunKwon@users.noreply.github.com>
2020-05-30 03:40:25 +0900

    add spring boot sample
    uplaod doc
    update uml

---
+++ src/ReligionSystem/src/main/java/com/rss/ReligionSystem/ReligionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rss.ReligionSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReligionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReligionSystemApplication.class, args);
+	}
+
+}
---
+++ src/ReligionSystem/src/test/java/com/rss/ReligionSystem/ReligionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rss.ReligionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReligionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
