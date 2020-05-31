e110301bc69bf73dceb6e4f2dbb7a5462ca3e2be
JeongJunKwon <37372602+JeongJunKwon@users.noreply.github.com>
2020-05-31 20:46:14 +0900

    create client project

---
+++ src/ReligionServer/src/main/java/com/rss/ReligionServer/ReligionServerApplication.java
@@ -0,0 +1,13 @@
+package com.rss.ReligionServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReligionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReligionServerApplication.class, args);
+	}
+
+}
---
+++ src/ReligionServer/src/test/java/com/rss/ReligionServer/ReligionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rss.ReligionServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReligionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
