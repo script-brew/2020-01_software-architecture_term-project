9ecc409e5e90bbb6614c3d7326b0fb5b49729a84
JeongJunKwon <37372602+JeongJunKwon@users.noreply.github.com>
2020-06-02 16:33:12 +0900

    init client settings

---
+++ src/ReligionClient/src/controller/SampleController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class SampleController {
+    public Button button;
+
+    public void find(ActionEvent actionEvent) {
+    }
+}
